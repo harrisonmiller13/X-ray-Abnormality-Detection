14e4043b155670986a4d96d0155212beeaf8bff1
harrisonmiller13 <harrison.miller13@gmail.com>
2019-09-06 11:18:13 -0400

    updated predict_app

--- predict_app.py
+++ predict_app.py
@@ -20,12 +20,12 @@ app = Flask(__name__)
 
 def get_model():
     global model
-    device = torch.device('cpu')
+    # device = torch.device('cpu')
     model = models.densenet121(pretrained=True)
     # model.load_state_dict(torch.load(PATHTOMODEL, map_location=device))
     model.eval()
     
-    print('* Model Loaded ヘ(◕。◕ヘ) ')
+    print('* Loaded PyTorch model 🔥ヘ(◕。◕ヘ) ')
 
 def preprocess_image(image_bytes):
     my_transforms = transforms.Compose([transforms.Resize(255),
@@ -38,22 +38,22 @@ def preprocess_image(image_bytes):
     image = Image.open(io.BytesIO(image_bytes))
     return my_transforms(image).unsqueeze(0)
 
-print("* Loading PyTorch model...")
 
 def get_prediction(image_bytes):
     tensor = preprocess_image(image_bytes=image_bytes)
     outputs = model.forward(tensor)
     _, y_hat = outputs.max(1)
     return y_hat
+
 get_model()
 
-@app.route("/predict",methods=["POST"])
+@app.route("/predict",methods=["GET","POST"])
 def predict():
     message = request.get_json(force=True)
     encoded = message['image']
-    decoded = base64.b64decode(encoded)
-    image = Image.open(io.BytesIO(decoded))
-    processed_image = preprocess_image(image,target_size = (224,224))
+    # decoded = base64.b64decode(encoded)
+    # image = Image.open(io.BytesIO(decoded))
+    processed_image = preprocess_image(image)
 
     prediction = model.eval(processed_image).tolist()
 
