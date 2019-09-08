0432408614d29d85a4f56f0b917c6862edc1e9e4
harrisonmiller13 <harrison.miller13@gmail.com>
2019-09-08 17:30:12 -0400

    renamed html templates

--- predtest.py
+++ predtest.py
@@ -1,6 +1,7 @@
 import matplotlib as plt
 import numpy as np
 import io
+import json
 
 import torch
 import torchvision
@@ -19,34 +20,43 @@ from flask import Flask, render_template
 
 
 def get_model():
-    path = 'model_retrieval.tar'
+    path = torch.load('model_retrieval.tar', 
+                        map_location=torch.device('cpu'))
     model = models.densenet201(pretrained=True)
-    model.classifier = nn.Linear(1920,1)
-    model.load_state_dict(torch.load(
-        path, map_location='cpu'),strict=False)
+    model.classifier = nn.Linear(1920,2)
+    model.load_state_dict(path['state_dict'],strict=False)
     model.eval()
+    print('* Loaded PyTorch model 🔥ヘ(◕。◕ヘ) ')
     return model
     
-    print('* Loaded PyTorch model 🔥ヘ(◕。◕ヘ) ')
+    
 
 def preprocess_image(image_bytes):
     my_transforms = transforms.Compose([transforms.Resize(255),
                                         transforms.CenterCrop(224),
                                         transforms.ToTensor(),
-                                        transforms.Lambda(lambda x: x.repeat(3,1,1)),
                                         transforms.Normalize(
                                             [0.485,0.456,0.406],
-                                            [0.229,0.224,0.255]
+                                            [0.229,0.224,0.225]
                                         )])
-    image = Image.open(io.BytesIO(image_bytes))
+    image = Image.open(io.BytesIO(image_bytes)).convert('RGB')
     return my_transforms(image).unsqueeze(0)
+
+with open('class_to_idx.json') as f:
+    class_to_result = json.load(f)
+
+idx_to_class = {v: k for k,v in class_to_result.items()}
 
 model = get_model()
 
 def get_inference(image_bytes):
     tensor = preprocess_image(image_bytes)
     outputs = model.forward(tensor)
-    print(outputs)
+    _, prediction = outputs.max(1)
+    category = prediction.item()
+    class_idx = idx_to_class[category]
+    return category,class_idx
+
 
 
 app = Flask(__name__)
@@ -54,15 +64,15 @@ app = Flask(__name__)
 @app.route('/predict', methods=['GET','POST'])
 def hello_world():
     if request.method == 'GET':
-        return render_template('predict2.html', value='hello')
+        return render_template('index.html', value='hello')
     if request.method == 'POST':
         if 'file' not in request.files:
             print("file not uploaded")
             return
         file = request.files['file']
         image = file.read()
-        get_inference(image_bytes=image)
-        return render_template('predict3.html', result='stuff')
+        category, class_idx = get_inference(image_bytes=image)
+        return render_template('result.html', xrayresult= class_idx, result=category)
 
 if __name__=='__main__':
     app.run(debug=True)
